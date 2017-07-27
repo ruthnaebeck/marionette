94b19ddaffd9467419dc7d47f49bae22c2e45c55
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-07-27 13:19:42 -0400

    2.5 Render from models

--- app/driver.js
+++ app/driver.js
@@ -1,23 +1,68 @@
 var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 
+var ToDoModel = require('./models/todo');
+
+
 var ToDo = Marionette.LayoutView.extend({
   tagName: 'li',
   template: require('./templates/todoitem.html')
 });
 
+
 var TodoList = Marionette.CompositeView.extend({
   el: '#app-hook',
   template: require('./templates/todolist.html'),
+
   childView: ToDo,
-  childViewContainer: 'ul'
+  childViewContainer: 'ul',
+
+  ui: {
+    assignee: '#id_assignee',
+    form: 'form',
+    text: '#id_text'
+  },
+
+  triggers: {
+    'submit @ui.form': 'add:todo:item'
+  },
+
+  collectionEvents: {
+    add: 'itemAdded'
+  },
+
+  modelEvents: {
+    change: 'render'
+  },
+
+  onAddTodoItem: function() {
+    this.model.set({
+      assignee: this.ui.assignee.val(),
+      text: this.ui.text.val()
+    });
+
+    if (this.model.isValid()) {
+      var items = this.model.pick('assignee', 'text');
+      this.collection.add(items);
+      console.log('Add to collection', items);
+    }
+  },
+
+  itemAdded: function() {
+    console.log('itemAdded');
+    this.model.set({
+      assignee: '',
+      text: ''
+    });
+  }
 });
 
 var todo = new TodoList({
   collection: new Backbone.Collection([
     {assignee: 'Scott', text: 'Write a book about Marionette'},
     {assignee: 'Andrew', text: 'Do some coding'}
-  ])
+  ]),
+  model: new ToDoModel()
 });
 
 todo.render();
