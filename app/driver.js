950c4053283a82d9927f4f7786e35b751e5f36ad
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-07-27 10:48:49 -0400

    2.4 Building a ToDo application

--- app/driver.js
+++ app/driver.js
@@ -1,10 +1,18 @@
+var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 
-var HelloWorld = Marionette.LayoutView.extend({
+var TodoList = Marionette.LayoutView.extend({
   el: '#app-hook',
   template: require('./templates/layout.html')
 });
 
-var hello = new HelloWorld();
+var todo = new TodoList({
+  model: new Backbone.Model({
+    items: [
+      {assignee: 'Scott', text: 'Write a book about Marionette'},
+      {assignee: 'Andrew', text: 'Do some coding'}
+    ]
+  })
+});
 
-hello.render();
+todo.render();
