de5de97ef1d37bd3f0d314208c2cf11de75316af
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-08-04 10:34:10 -0400

    Started ES6 refactor

--- app/driver.js
+++ app/driver.js
@@ -1,18 +1,34 @@
-var Backbone = require('backbone');
-var Marionette = require('backbone.marionette');
-var TodoView = require('./views/layout');
-var ToDoModel = require('./models/todo');
+import Backbone from 'backbone';
+import Marionette from 'backbone.marionette';
 
-var initialData = [
+import TodoView from './views/layout';
+import TodoModel from './models/todo';
+
+const initialData = [
   {assignee: 'Scott', text: 'Write a book about Marionette'},
   {assignee: 'Andrew', text: 'Do some coding'}
 ];
+
+export class TodoApp extends Marionette.Application
+{
+  constructor(options)
+  {
+    super(options);
+
+    this.todoModel = new TodoModel({
+      app: this
+    });
+
+    this.todoCollection = new Backbone.Collection(options.initialData);
+
+  }
+}
 
 var app = new Marionette.Application({
   onStart: function(options){
     var todo = new TodoView({
       collection: new Backbone.Collection(options.initialData),
-      model: new ToDoModel()
+      model: new TodoModel()
     });
     todo.render();
     todo.triggerMethod('show');
--- app/models/todo.js
+++ app/models/todo.js
@@ -1,3 +1,40 @@
+// import Backbone from 'backbone';
+
+// export class TodoModel extends Backbone.Model
+// {
+//   constructor(options)
+//   {
+//     super(options);
+//     // this.app = options.app;
+//   }
+
+//   defaults()
+//   {
+//     return {
+//       assignee: '',
+//       text: ''
+//     };
+//   }
+
+//   validate(attrs)
+//   {
+//     const errors = {};
+//     let hasError = false;
+//     if (!attrs.assignee)
+//     {
+//       errors.assignee = 'assignee must be set';
+//       hasError = true;
+//     }
+//     if (!attrs.text)
+//     {
+//       errors.text = 'text must be set';
+//       hasError = true;
+//     }
+//     console.log('errors', errors);
+//     if (hasError) return errors;
+//   }
+// }
+
 var Backbone = require('backbone');
 
 var ToDo = Backbone.Model.extend({
